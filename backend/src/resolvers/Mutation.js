76cdbdfa8f798e2ca4e91ed07d1159a1199ef3c7
Adrian Kawanishi <adriankawanishi@gmail.com>
2019-01-07 10:34:37 -0800

    Setup mailtrap and send email with reset token

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -91,6 +92,7 @@ const Mutations = {
     ctx.response.clearCookie('token');
     return { message: 'Goodbye!'};
   },
+
   async requestReset(parent, args, ctx, info) {
     // 1. Check if this is a real user
     const user = await ctx.db.query.user({ where: {email: args.email }});
@@ -105,9 +107,19 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpiry }
     })
-    return { message: 'Thanks!'};
     // 3. Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: 'adrian@example.com',
+      to: user.email,
+      subject: 'Your password reset token!',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+        \n\n
+        <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset<a/>`),
+    });
+    // 4. Return the message
+    return { message: 'Thanks!'};
   },
+
   async resetPassword(parent, args, ctx, info) {
     // 1. Check if passwords match
     if(args.password !== args.confirmPassword) {
