184f0cb1202c226c6aa4a9601c34baf7decb6b5e
aknishi <adriankawanishi@gmail.com>
2019-06-04 16:50:16 -0700

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ server.express.use(cookieParser());
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
-    const {userId} = jwt.verify(token, process.env.APP_SECRET);
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
     // put the userID onto the req for future requests to access
     req.userId = userId;
   }
@@ -22,15 +22,15 @@ server.express.use((req, res, next) => {
 // 2. Create a middleware that populates the user on each request
 server.express.use(async (req, res, next) => {
   // if they are not logged in, skip this
-  if(!req.userId) return next();
+  if (!req.userId) return next();
   const user = await db.query.user(
-    { where: { id: req.userId }},
+    { where: { id: req.userId } },
     '{ id, permissions, email, name }'
   );
   req.user = user;
   next();
 });
-//start it!
+
 server.start(
   {
     cors: {
